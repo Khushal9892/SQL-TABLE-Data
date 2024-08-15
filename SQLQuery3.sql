03aab35b4d6c419b01ae49a1843b32a86e693c10
Khushal Sarvaiya <156545455+Khushal9892@users.noreply.github.com>
2024-08-14 21:12:58 -0400

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+insert into EmployeeDemographic values
+(3001, 'Jeena','Mohate',35,'Female'),
+(3002, 'Sangita','Kadam', 30,'Female'),
+(3003, 'Priya','Kadam', 32,'Female'),
+(3004, 'Priyanka','Kadam',33,'Female'),
+(3005, 'Bharati', 'More',33,'Female'),
+(3006, 'Prajakata','More',35,'Female'),
+(3007,' Khushi','Darji',28,'Female'),
+(3008,'Khushabu','Rathod',28,'Female'),
+(3009,'Kriya','Kapoor',28,'Female'),
+(3010,'JIya','Kapoor',29,'Female')
+
